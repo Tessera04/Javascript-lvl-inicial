49c8a40094be4278f1444f5d5b05a0c0a37b0db2
Matias Gonzalez <matute_11@hotmail.com.ar>
2022-10-29 03:06:20 -0300

    Primer commit

---
+++ 02-nombreVariables.js
@@ -0,0 +1,5 @@
+var nombreEmpleado = "Juan Antonio";
+
+let costoPasaje = 150;
+
+const direccion = "Flores 154 - Timbre 3";
---
+++ 03-buenasPracticas.js
@@ -0,0 +1,15 @@
+let nombreEmpleado = 'Jose Perez';
+
+let NombreEmpleado = 'Raul Gimenez';
+
+console.log(nombreEmpleado);
+
+console.log(NombreEmpleado);
+
+let edadEmpleado = 40;
+
+console.log(edadEmpleado);
+
+edadEmpleado = 'Cuarenta';
+
+console.log(edadEmpleado);
