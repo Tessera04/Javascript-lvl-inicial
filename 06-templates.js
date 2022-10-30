02483fe2eadf811184ca0c6de55b733fa5d4d64c
Matias Gonzalez <matute_11@hotmail.com.ar>
2022-10-30 03:09:40 -0300

    templates

---
+++ 06-templates.js
@@ -0,0 +1,11 @@
+const nombrePasajero = 'Leonardo';
+const apellidoPasajero = 'Anal';
+
+let nombreCompleto = `El nombre completo es: ${nombrePasajero} ${apellidoPasajero}`;
+console.log(nombreCompleto);
+
+let segundoPasajero = "Diego Castillo";
+console.log(`El nombre del segundo pasajero es: ${segundoPasajero}`);
+segundoPasajero = 1214332542;
+console.log(`El nombre del segundo pasajero es: ${segundoPasajero}`);
+//Javascript deja sobreescribir la variable independientemente de si es string, booleano, numero, etc.
